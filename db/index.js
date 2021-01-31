5b48b99e9c949e0a23708596986772b77d84101c
qihong1983 <55377146@qq.com>
2021-01-31 22:48:21 +0800

    first commit

---
+++ db/index.js
@@ -0,0 +1,234 @@
+const mysql = require('mysql');
+const moment = require('moment');
+
+
+const pool = mysql.createPool({
+    connectionLimit: 10,
+    password: '123456',
+    user:'root',
+    database: 'studentManage',
+    host: '43.252.230.6',
+    port: '3306'
+});
+
+
+
+let dbObj = {};
+
+
+dbObj.one = (studentName, type) => {
+    return new Promise((resolve,reject)=> {
+
+        // console.log(`SELECT * FROM task where studentname = ${studentName} and type = ${type} and createAt = ${moment().format("YYYYMMDD")}`);
+
+        pool.query(`SELECT * FROM task where studentname = '${studentName}' and type = ${type} and createAt = '${moment().format("YYYYMMDD")}'`, (err, results) => {
+            if (err) {
+                return reject(err);
+            }
+    
+            return resolve(results);
+        });
+
+    })
+}
+
+
+dbObj.insertTask = (studentName, type, filename) => {
+    return new Promise((resolve,reject)=> {
+
+        // console.log(`SELECT * FROM task where studentname = ${studentName} and type = ${type} and createAt = ${moment().format("YYYYMMDD")}`);
+
+        console.log(`INSERT INTO task (type, studentname, url,createAt) VALUES (${type}, '${studentName}', '${filename}','${moment().format("YYYYMMDD")}');`);
+        pool.query(`INSERT INTO task (type, studentname, url,createAt) VALUES (${type}, '${studentName}', '${filename}','${moment().format("YYYYMMDD")}');`, (err, results) => {
+            if (err) {
+                return reject(err);
+            }
+    
+            return resolve(results);
+        });
+
+    })
+}
+
+
+dbObj.updateTask = (studentName, type, filename) => {
+    return new Promise((resolve,reject)=> {
+
+        // console.log(`SELECT * FROM task where studentname = ${studentName} and type = ${type} and createAt = ${moment().format("YYYYMMDD")}`);
+
+       
+        console.log(`UPDATE task SET url = '${filename}' where studentname = '${studentName}' and type = ${type} and createAt = '${moment().format("YYYYMMDD")}`);
+        pool.query(`UPDATE task SET url = '${filename}' where studentname = '${studentName}' and type = ${type} and createAt = '${moment().format("YYYYMMDD")}'`, (err, results) => {
+            if (err) {
+                return reject(err);
+            }
+    
+            return resolve(results);
+        });
+
+    })
+}
+
+
+dbObj.all = (studentName, type, filename) => {
+
+    return new Promise((resolve,reject)=> {
+        console.log(`select student.id, student.studentname, task.url, task.type from student left join task on student.studentname = task.studentname and task.createAt = '${moment().format("YYYYMMDD")}';`);
+      
+        pool.query(`select student.id, student.studentname, task.url from student left join task on student.studentname = task.studentname and task.createAt = '${moment().format("YYYYMMDD")}';`, (err, results) => {
+            if (err) {
+                return reject(err);
+            }
+    
+            return resolve(results);
+        });
+    })
+    
+}
+
+//技术类
+
+
+
+
+dbObj.oneJishu = (studentName, type) => {
+    return new Promise((resolve,reject)=> {
+
+        // console.log(`SELECT * FROM task where studentname = ${studentName} and type = ${type} and createAt = ${moment().format("YYYYMMDD")}`);
+
+        pool.query(`SELECT * FROM task2 where studentname = '${studentName}' and type = ${type} and createAt = '${moment().format("YYYYMMDD")}'`, (err, results) => {
+            if (err) {
+                return reject(err);
+            }
+    
+            return resolve(results);
+        });
+
+    })
+}
+
+
+dbObj.insertTaskJishu = (studentName, type, filename) => {
+    return new Promise((resolve,reject)=> {
+
+        // console.log(`SELECT * FROM task where studentname = ${studentName} and type = ${type} and createAt = ${moment().format("YYYYMMDD")}`);
+
+        console.log(`INSERT INTO task (type, studentname, url,createAt) VALUES (${type}, '${studentName}', '${filename}','${moment().format("YYYYMMDD")}');`);
+        pool.query(`INSERT INTO task2 (type, studentname, url,createAt) VALUES (${type}, '${studentName}', '${filename}','${moment().format("YYYYMMDD")}');`, (err, results) => {
+            if (err) {
+                return reject(err);
+            }
+    
+            return resolve(results);
+        });
+
+    })
+}
+
+
+
+
+
+
+dbObj.updateTaskJishu = (studentName, type, filename) => {
+    return new Promise((resolve,reject)=> {
+
+        // console.log(`SELECT * FROM task where studentname = ${studentName} and type = ${type} and createAt = ${moment().format("YYYYMMDD")}`);
+
+       
+        console.log(`UPDATE task2 SET url = '${filename}' where studentname = '${studentName}' and type = ${type} and createAt = '${moment().format("YYYYMMDD")}`);
+        pool.query(`UPDATE task2 SET url = '${filename}' where studentname = '${studentName}' and type = ${type} and createAt = '${moment().format("YYYYMMDD")}'`, (err, results) => {
+            if (err) {
+                return reject(err);
+            }
+    
+            return resolve(results);
+        });
+
+    })
+}
+
+
+
+
+
+
+
+dbObj.allJishu = (studentName, type, filename) => {
+    return new Promise((resolve,reject)=> {
+        console.log(`select student.id, student.studentname, task.url, task.type from student left join task on student.studentname = task.studentname and task.createAt = '${moment().format("YYYYMMDD")}';`);
+        pool.query(`select student.id, student.studentname, task2.url from student left join task2 on student.studentname = task2.studentname and task2.createAt = '${moment().format("YYYYMMDD")}';`, (err, results) => {
+            if (err) {
+                return reject(err);
+            }
+    
+            return resolve(results);
+        });
+    })
+    
+}
+
+
+
+
+
+
+
+dbObj.insertStu = (studentName, studentPhone) => {
+    return new Promise((resolve,reject)=> {
+
+        // console.log(`SELECT * FROM task where studentname = ${studentName} and type = ${type} and createAt = ${moment().format("YYYYMMDD")}`);
+
+        console.log(`INSERT INTO student (studentname, studentphone) VALUES ('${studentName}', '${studentPhone});`);
+        pool.query(`INSERT INTO student (studentname, studentphone) VALUES ('${studentName}', '${studentPhone}');`, (err, results) => {
+            if (err) {
+                return reject(err);
+            }
+    
+            return resolve(results);
+        });
+
+    })
+}
+
+
+dbObj.selectStu = () => {
+    return new Promise((resolve,reject)=> {
+
+        // console.log(`SELECT * FROM task where studentname = ${studentName} and type = ${type} and createAt = ${moment().format("YYYYMMDD")}`);
+
+        // console.log(`INSERT INTO student (studentname, studentphone) VALUES ('${studentName}', '${studentPhone});`);
+        pool.query(`SELECT * FROM student`, (err, results) => {
+            if (err) {
+                return reject(err);
+            }
+    
+            return resolve(results);
+        });
+
+    })
+}
+
+
+dbObj.deleteStu = (id) => {
+    return new Promise((resolve,reject)=> {
+
+        // console.log(`SELECT * FROM task where studentname = ${studentName} and type = ${type} and createAt = ${moment().format("YYYYMMDD")}`);
+
+        // console.log(`INSERT INTO student (studentname, studentphone) VALUES ('${studentName}', '${studentPhone});`);
+        pool.query(`delete from student where id = ${id};`, (err, results) => {
+            if (err) {
+                return reject(err);
+            }
+    
+            return resolve(results);
+        });
+
+    })
+}
+
+
+
+
+
+module.exports = dbObj;
---
+++ index.js
@@ -0,0 +1,266 @@
+const express = require('express');
+const upload =require('express-fileupload');
+var bodyParser = require('body-parser');
+const app = express ();
+const db = require("./db/index");
+
+const moment = require('moment');
+
+
+const cors=require('cors')
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+
+
+
+app.use(upload())
+
+// app.get('/', (req, res) => {
+//     res.sendFile(__dirname + '/');
+// })
+
+app.use(express.static(__dirname + '/public'));
+
+
+app.post('/jiaoyu',  (req,res) => {
+    
+    if (req.files) {
+    
+        var file = req.files.file;
+        var filename = file.name;
+
+        
+        const {studentName,type} = req.body;
+
+        
+        if (!studentName) {
+            res.json({status: false, msg: "用户名错误"});
+            return ;
+        }
+        
+
+        file.mv('./public/uploads/'+filename, async function (err) {
+           
+            if (err) {
+                res.send(err);
+            } else {
+                
+
+                
+                
+                    let resultSelect = await db.one(studentName, type);
+                
+                    // res.json(resultSelect);
+
+                    if (!resultSelect.length) {
+                        let insertTask = await db.insertTask(studentName, type, filename);
+                        
+                    } else {
+                        let updateTask = await db.updateTask(studentName, type, filename);
+                    
+                    }
+
+                
+                    
+                    let results =  await db.all(studentName, type, filename);
+
+                    let jsonData = {
+                        status: true,
+                        data:results 
+                    }
+                    res.json(jsonData);
+                
+            }
+        })
+    }
+});
+
+
+
+
+
+// 技术类的
+app.post('/jishu',  (req,res) => {
+    
+    if (req.files) {
+    
+        var file = req.files.file;
+        var filename = file.name;
+
+        
+        const {studentName,type} = req.body;
+
+        
+        if (!studentName) {
+            res.json({status: false, msg: "用户名错误"});
+            return ;
+        }
+        
+
+        file.mv('./public/uploadsjishu/'+filename, async function (err) {
+           
+            if (err) {
+                res.send(err);
+            } else {
+                
+
+                
+                
+                    let resultSelect = await db.oneJishu(studentName, type);
+                
+                    // res.json(resultSelect);
+
+                    if (!resultSelect.length) {
+                        let insertTask = await db.insertTaskJishu(studentName, type, filename);
+                        
+                    } else {
+                        let updateTask = await db.updateTaskJishu(studentName, type, filename);
+                    }
+
+                
+                    let results =  await db.allJishu(studentName, type, filename);
+
+                    let jsonData = {
+                        status: true,
+                        data:results 
+                    }
+
+                    res.json(jsonData);
+            }
+        })
+    }
+});
+
+
+//教育初始化
+
+app.post('/jiaoyuinit',  async (req,res) => {
+    
+  
+                
+                    
+                    let results =  await db.all('', 0, '');
+
+                    let jsonData = {
+                        status: true,
+                        data:results 
+                    }
+                    res.json(jsonData);
+                
+    
+});
+
+
+//教育初始化
+
+app.post('/jishuinit',  async (req,res) => {              
+    let results =  await db.allJishu('', 1, '');
+
+    // let results =  await db.allJishu(studentName, type, filename);
+
+
+    let jsonData = {
+        status: true,
+        data:results 
+    }
+    res.json(jsonData);
+});
+
+
+
+app.post('/insertStu',  async (req,res) => {   
+
+
+
+
+    const {studentname,studentphone} = req.body;
+
+    try {
+
+        let results =  await db.insertStu(studentname,studentphone);
+
+        // let results =  await db.allJishu(studentName, type, filename);
+    console.log(111);
+    
+        let jsonData = {
+            status: true
+        }
+        res.json(jsonData);
+
+    } catch(e) {
+
+        res.status(200).json({
+            status: false
+        });
+    }
+    
+});
+
+
+
+app.post('/selectStu',  async (req,res) => {   
+
+
+
+
+    // const {studentname,studentphone} = req.body;
+
+    try {
+
+        let results =  await db.selectStu();
+
+        // let results =  await db.allJishu(studentName, type, filename);
+    
+    
+        let jsonData = {
+            status: true,
+            data:results 
+        }
+        res.json(jsonData);
+
+    } catch(e) {
+console.log(e);
+        res.status(200).json({
+            status: false
+        });
+    }
+    
+});
+
+
+app.post('/deleteStu',  async (req,res) => {   
+
+
+
+
+    const {id} = req.body;
+
+    try {
+
+        let results =  await db.deleteStu(id);
+
+        // let results =  await db.allJishu(studentName, type, filename);
+    
+    
+        let jsonData = {
+            status: true,
+            data:results 
+        }
+        res.json(jsonData);
+
+    } catch(e) {
+console.log(e);
+        res.status(200).json({
+            status: false
+        });
+    }
+    
+});
+
+
+
+app.listen(8000, ()=> {
+    console.log('启动http服务:8000端口');
+});
